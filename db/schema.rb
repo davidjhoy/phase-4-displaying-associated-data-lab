b63a1cd622aa62321071862c505eef4ead49d1fd
david <davidjhoyjr@gmail.com>
2022-07-18 13:47:29 -0400

    commit tree 3023f24e31854b41f2b424f7f90134823ae9c125

--- db/schema.rb
+++ db/schema.rb
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 2021_05_10_111617) do
     t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_items_on_user_id"
+    t.index "user_id", name: "index_items_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
